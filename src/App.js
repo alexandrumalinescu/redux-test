18abb7906002e0e491ed9566286626abc8ac4444
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-08-04 18:43:07 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import CakeContainer from './components/CakeContainer';
 import {Provider} from 'react-redux';
 import store from './redux/store';
+import HookCakeContainer from './components/HookCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
 
 function App() {
 
@@ -12,6 +15,9 @@ function App() {
     <div className="App">
      <Provider store={store}>
       <CakeContainer/>
+      <HookCakeContainer/>
+      <IceCreamContainer/>
+      <NewCakeContainer/>
      </Provider>
       
     </div>
